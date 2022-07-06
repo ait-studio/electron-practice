ad41a5caa0a16be327291b6b6b0e65fbb4c16ce0
hajun <audgkwns44@gmail.com>
2022-07-06 16:33:43 +0900

    [Create]install esential packages - cors, express, axios

--- back/src/app.js
+++ back/src/app.js
@@ -1,11 +1,5 @@
 import cors from "cors";
 import express from "express";
-import { login_required } from "./middlewares/login_required.js";
-import { userRouter } from "./routers/userRouter.js";
-import { worldRouter } from "./routers/worldRouter.js";
-import { mainRouter } from "./routers/mainRouter.js";
-import { detailRouter } from "./routers/detailRouter.js";
-import { myPageRouter } from "./routers/myPageRouter.js";
 
 const app = express();
 
